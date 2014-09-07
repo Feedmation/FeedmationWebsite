8699a52530e46c8f0f14e59b5ea3d66af0e16288
Josh Hartley <hartleyjr@missouri.edu>
2014-09-07 18:53:35 -0500

    Web Service Update
    -pull_settings is not connected to the database

--- api/v1/sync_data.php
+++ api/v1/sync_data.php
@@ -63,6 +63,54 @@ if(!$stmt)
  			
  			// function for providing per feeder with tag settings
  			case 'pull_settings':
+ 			
+ 				//Start query to get tag settings for a feeder
+				$tagQuery = "SELECT * FROM $GLOBALS[schema].rfid WHERE feeder_id = $1";
+				$stmt = pg_prepare($dbConn,"tags",$tagQuery);
+		
+				echo pg_result_error($stmt);
+
+				//if statement won't prepare then return error else execute statment
+				if(!$stmt) {
+				
+					header('Content-Type: application/json');
+					echo json_encode(array("error" => "database query error"));
+					return;
+
+				} else {
+	
+					$tagResults =  pg_execute($dbConn,"tags",array($feederid));
+					if(pg_num_rows($tagResults)==0) {
+					
+						header('Content-Type: application/json');
+			    		echo json_encode(array("error" => "no tags available for given feeder id"));
+			    			
+					} else {
+						
+						if(pg_num_rows($tagResults) > 0) {
+							
+							$tagStack;
+							while($tagRow = pg_fetch_assoc($tagResults)) {
+					
+								$tagChange = $tagRow['has_changed'];
+								$tagID = $tagRow['tag_id'];
+								$amount = $tagRow['feed_amount'];
+								$slot1Start = $tagRow['slot_one_start'];
+								$slot1End = $tagRow['slot_one_end'];
+								$slot2Start = $tagRow['slot_two_start'];
+								$slot2End = $tagRow['slot_two_end'];
+								
+								$tag = new Tag($tagChange, $tagID, $amount, $slot1Start, $slot1End, $slot2Start, $slot2End);
+								array_push($tagStack, $tag->getArray());
+							}
+							
+							header('Content-Type: application/json');
+			   				echo json_encode($tagStack);
+						}
+					}
+				}
+					
+ 				/*
 				$tag1 = new Tag(true, '84003515CA', 3.5, 10, 11, 16, 20);
 				$tag2 = new Tag(true, '84003515CA', 2.5, 10, 12, 16, 21);
 				$tag3 = new Tag(false, '84003515CA', 1.5, 10, 11, 16, 22);
@@ -71,6 +119,7 @@ if(!$stmt)
 				
 				header('Content-Type: application/json');
 			    echo json_encode($tags);
+			    */
 			    
 			break;
       
