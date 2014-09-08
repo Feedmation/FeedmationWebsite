e476f6871154a69d3f6808dfbd7f1e0ff7dc1adf
Josh Hartley <hartleyjr@missouri.edu>
2014-09-07 19:35:11 -0500

    Web Service update

--- api/v1/sync_data.php
+++ api/v1/sync_data.php
@@ -102,7 +102,7 @@ if(!$stmt)
 								$slot2End = $tagRow['slot_two_end'];
 								
 								$tag = new Tag($tagChange, $tagID, $amount, $slot1Start, $slot1End, $slot2Start, $slot2End);
-								array_push($tagStack, array ($tagCount => $tag->getArray()));
+								array_push($tagStack, $tagCount => $tag->getArray());
 								$tagCount++;
 							}
 							
