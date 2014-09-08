4af811450f752263c63246ba7bf713191a51d092
Josh Hartley <hartleyjr@missouri.edu>
2014-09-07 20:33:34 -0500

    Web Service update

--- api/v1/sync_data.php
+++ api/v1/sync_data.php
@@ -89,7 +89,7 @@ if(!$stmt)
 						
 						if(pg_num_rows($tagResults) > 0) {
 							
-							$tagStack = array();
+							$tagArray = array();
 							$tagCount = 1;
 							while($tagRow = pg_fetch_assoc($tagResults)) {
 					
@@ -102,12 +102,12 @@ if(!$stmt)
 								$slot2End = $tagRow['slot_two_end'];
 								
 								$tag = new Tag($tagChange, $tagID, $amount, $slot1Start, $slot1End, $slot2Start, $slot2End);
-								array_push($tagStack, $tagCount => $tag->getArray());
+								$tagArray[$tagCount] = $tag;
 								$tagCount++;
 							}
 							
 							header('Content-Type: application/json');
-			   				echo json_encode($tagStack);
+			   				echo json_encode($tagArray);
 						}
 					}
 					
