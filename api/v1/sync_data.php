1f794ea7433687dda2f51c4196f49a39c078849d
Josh Hartley <hartleyjr@missouri.edu>
2014-09-19 12:07:23 -0500

    test

--- api/v1/sync_data.php
+++ api/v1/sync_data.php
@@ -108,7 +108,7 @@ if(!$stmt)
 								}
 								
 								$tag = new Tag($tagChange, $tagID, $amount, $slot1Start, $slot1End, $slot2Start, $slot2End);
-								$tagArray[$slotNum] = $slotNum => $tag->getArray();
+								$tagArray[$slotNum] = $tag->getArray();
 								
 								/*
 								//Now that the JSON has been sent to the feeder, set all it's tags has_chaged values to false
