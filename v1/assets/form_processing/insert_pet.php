761c19bc987f11c11f999330a538d12991161f25
Josh Hartley <hartleyjr@missouri.edu>
2014-09-19 10:40:01 -0500

    Insert pet update

--- v1/assets/form_processing/insert_pet.php
+++ v1/assets/form_processing/insert_pet.php
@@ -48,7 +48,7 @@ include_once '../../loginFunctions.php';
 		} else 
 		{ //else locating an open slot between 1 and 4
 		
-			$openSlot = NULL;
+			$openSlot = 0;
 			$match = NULL;
 			$keepSearching = true;
 			
@@ -81,47 +81,43 @@ include_once '../../loginFunctions.php';
 			
 			pg_free_result($slotAmountResult);
 		
-			if ( $openSlot != NULL ) {
-				//check if the pet tag already exists in the DB
-				$tagIdSelect = "SELECT * FROM $GLOBALS[schema].rfid WHERE tag_id = $1 AND user_email = $2";
-	
-				$tagIdPrep = pg_prepare($dbConn, "tagSelect", $tagIdSelect);
-	
-				if($tagIdPrep) {
-					$tagIdResult = pg_execute($dbConn, "tagSelect", array($tagNumber, $_SESSION['user']));
-				
-					if(pg_num_rows($tagIdResult) == 0) {
-		
-						//no match found, proceed with insert	
-						$petInsert = "INSERT INTO $GLOBALS[schema].rfid VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)";
-		
-						$petPrep = pg_prepare($dbConn, "insertPet", $petInsert);
-		
-						if($petPrep) {
-							$petResult = pg_execute($dbConn, "insertPet", array($tagNumber, $feederId, $startSlot1, $endSlot1, $startSlot2, $endSlot2, $petName, $_SESSION['user'], "TRUE", $feedAmt, $openSlot));	
-							pg_free_result($petResult);
-						} else {
-							echo "<p>Couldn't insert values for pet. Try again later.</p>";
-						}
+			//check if the pet tag already exists in the DB
+			$tagIdSelect = "SELECT * FROM $GLOBALS[schema].rfid WHERE tag_id = $1 AND user_email = $2";
 
-						//this function will refresh the list of feeders 
-						//by querying the DB and printing out the formatted HTML.
-						//it is located in assets/php_functions/phpFunctions.php
-						$data = populateFeeders();
-						echo $data;
+			$tagIdPrep = pg_prepare($dbConn, "tagSelect", $tagIdSelect);
+
+			if($tagIdPrep) {
+				$tagIdResult = pg_execute($dbConn, "tagSelect", array($tagNumber, $_SESSION['user']));
 		
+				if(pg_num_rows($tagIdResult) == 0) {
+
+					//no match found, proceed with insert	
+					$petInsert = "INSERT INTO $GLOBALS[schema].rfid VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)";
+
+					$petPrep = pg_prepare($dbConn, "insertPet", $petInsert);
+
+					if($petPrep) {
+						$petResult = pg_execute($dbConn, "insertPet", array($tagNumber, $feederId, $startSlot1, $endSlot1, $startSlot2, $endSlot2, $petName, $_SESSION['user'], "TRUE", $feedAmt, $openSlot));	
+						pg_free_result($petResult);
 					} else {
-						//the feederId already exists. reprint the form
-						//and display an error message
-						echo "petExists";
-			
+						echo "<p>Couldn't insert values for pet. Try again later.</p>";
 					}
-				
+
+					//this function will refresh the list of feeders 
+					//by querying the DB and printing out the formatted HTML.
+					//it is located in assets/php_functions/phpFunctions.php
+					$data = populateFeeders();
+					echo $data;
+
 				} else {
-					echo "<p>Couldn't sanitize query for Tag ID.</p>";
+					//the feederId already exists. reprint the form
+					//and display an error message
+					echo "petExists";
+	
 				}
+		
 			} else {
-				echo "<p>Problem finding slot for pet</p>";
+				echo "<p>Couldn't sanitize query for Tag ID.</p>";
 			}
 		}
 		
