3facc5cf48699d39b01dc4a0e57e956b40e26244
Feedmation <jackson.wright69@gmail.com>
2014-10-01 00:38:29 -0500

    4tr

--- v1/dPet.php
+++ v1/dPet.php
@@ -39,40 +39,9 @@
 		<br><br>
 		
 		<?php 
-			//feeder value
 			$selected_Fdr = $_POST['feederId'];
-			
-			
-		$selectPets = "SELECT * FROM $GLOBALS[schema].rfid WHERE user_email = $1 and feeder_id = $2";
-		
-		$selectPetsPrep = pg_prepare($dbconn, "pets", $selectPets);
-		
-		if($selectPetsPrep) {
-			$petsResult = pg_execute($dbconn, "pets", array($_SESSION['user'], $selected_Fdr);
-		} else {
-			echo "Could not sanitize user name. Try again later.";
-		}
-		
-		if($petsResult) {
-			$pets = '';
-			$i = 0;
-			while($row = pg_fetch_assoc($petsResult)) {
-				$pets.= "	<option value='$row[tag_id]'>$row[pet_name]</option>  ";			
-				$i++;
-			}
-			pg_free_result($petsResult);
-		//	echo $pets;
-			
-		} else {
-			echo "Could not query for Pets. Try refreshing the page";
-		}	
-		?>
-		
-<label for='Pet'>Select a Pet:</label>
-		<br>
-		<select name="petId" required="required" class="form-control" id="petId">		
-		<?php $pets ?>
-		</select>
+			echo $selected_Fdr;
+			?>
 		<br><br>
 			<center><br><a href="index.php">Feedmation</a><br><br></center>
 
