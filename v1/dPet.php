7d661ed59ac76471ecef72f5babdff0b7f80eaf9
Feedmation <jackson.wright69@gmail.com>
2014-10-01 01:21:35 -0500

    FDR

--- v1/dPet.php
+++ v1/dPet.php
@@ -63,8 +63,6 @@
 				$i++;
 			}
 			pg_free_result($feedersResult);
-		///	echo $feeders;
-			
 		} else 
 		{
 			echo "Could not query for Pet Feeders. Try refreshing the page";
@@ -72,6 +70,47 @@
 		$selected_Fdr = $_POST['feederId'];
 		
 		echo "Your selected value is " .$selected_Fdr;
+		?>
+		
+		<?
+		$selectPets = "SELECT * FROM $GLOBALS[schema].rfid WHERE user_email = $1 and feeder_id = $2";
+		
+		$selectPetsPrep = pg_prepare($dbconn, "pets", $selectPets);
+		
+		if($selectPetsPrep) {
+			$petsResult = pg_execute($dbconn, "pets", array($_SESSION['user'], $selected_Fdr));
+		} else {
+			echo "Could not sanitize user name. Try again later.";
+		}
+		
+		if($petsResult) 
+			{
+			$pets = '';
+			$i = 0;
+			while($row = pg_fetch_assoc($petsResult)) 
+		{
+				$pets.= "
+							<option value='$row[tag_id]'>$row[pet_name]</option>";
+
+?>
+<label for='Feeder'>Select a Pet:</label>
+		<br>
+		<select name="petId" required="required" class="form-control" id="petId">
+		<?echo $feeders ?>
+				</select>
+		<br><br>
+<?							
+				$i++;
+		}
+			}
+			else {
+			echo "Could not query for Pets. Try refreshing the page";
+		}	
+		?>
+		
+
+		
+		
 ?>
 		<br><br>
 			<center><br><a href="index.php">Feedmation</a><br><br></center>
