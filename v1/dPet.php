3f073ff2669fa14f206099bdc5cb1492f8a8301d
Feedmation <jackson.wright69@gmail.com>
2014-10-01 00:04:06 -0500

    dPEt get

--- v1/dPet.php
+++ v1/dPet.php
@@ -31,48 +31,18 @@
 
 <br>
 
-<label for='Pet'>Select a Feeder:</label>
+<label for='Feeder'>Select a Feeder:</label>
 		<br>
 		<select name="feederId" required="required" class="form-control" id="feederId">
 			<?php populateFeedersSelectBox(); ?>
 		</select>
 		<br><br>
-	<?php
-	
-		$dbconn = dbconnect();
-	
-		
-		$dbconn = dbconnect();
-	
-		$selectPets = "SELECT * FROM $GLOBALS[schema].rfid WHERE user_email = $1 and feeder_id = $2";
-		
-		$selectPetsPrep = pg_prepare($dbconn, "pets", $selectPets);
-		
-		if($selectPetsPrep) {
-			$petsResult = pg_execute($dbconn, "pets", array($_SESSION['user'], $_GET['feederId']));
-		} else {
-			echo "Could not sanitize user name. Try again later.";
-		}
-		
-		if($petsResult) {
-			$pets = '';
-			$i = 0;
-			while($row = pg_fetch_assoc($petsResult)) {
-				$pets.= "
-							<option value='$row[tag_id]'>$row[pet_name]</option>
-						   ";			
-				$i++;
-			}
-			pg_free_result($petsResult);
-			echo $pets;
-			
-		} else {
-			echo "Could not query for Pets. Try refreshing the page";
-		}	
-	
-	
-	
-	?>
+<label for='Pet'>Select a Pet:</label>
+		<br>
+		<select name="petId" required="required" class="form-control" id="petId">		
+		<?php populatePetsSelectBox()?>
+		</select>
+		<br><br>
 			<center><br><a href="index.php">Feedmation</a><br><br></center>
 
 		
