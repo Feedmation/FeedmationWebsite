08376521b8c70fe9d48ccaa6f8bab9290650eb3a
Feedmation <jackson.wright69@gmail.com>
2014-10-06 15:32:25 -0500

    get value

--- v1/dPet.php
+++ v1/dPet.php
@@ -75,6 +75,7 @@ $('select').change(function() '
 	<br><br>
 	
 	<?
+	
 		$feederID = $_POST['feeder'];
 		
 		if($feederID!=5)
@@ -84,7 +85,39 @@ $('select').change(function() '
 	<!--Pets-->
 	<label for='pet'>Select a Pet:</label>
 	<select class="form-control" id='petSelect' name='pet'>
-		<?php populatePetsSelectBox(); ?>
+		<?php populatePetsSelectBox();
+
+	$selectPets = "SELECT * FROM $GLOBALS[schema].rfid WHERE user_email = $1 and feeder_id = $2";
+		
+		$selectPetsPrep = pg_prepare($dbconn, "pets", $selectPets);
+		
+		if($selectPetsPrep) {
+			$petsResult = pg_execute($dbconn, "pets", array($_SESSION['user'], $feederID));
+		} else {
+			echo "Could not sanitize user name. Try again later.";
+		}
+		
+		if($petsResult)
+		{
+			$pets = '';
+			$i = 0;
+			while($row = pg_fetch_assoc($petsResult)) {
+				$pets.= "
+							<option value='$row[tag_id]'>$row[pet_name]</option>
+						   ";			
+				$i++;
+			}
+			pg_free_result($petsResult);
+			?>
+			
+			<label for='pet'>Select a Pet:</label>
+	<select class="form-control" id='petSelect' name='pet'>
+		<?php 
+			echo $pets;	
+		} else {
+			echo "Could not query for Pets. Try refreshing the page";
+		}	
+		?>
 	</select>
 	<br><br>
 			<center><button type='submit' name='deletePet' class='btn btn-default'>Delete Pet From Feeder</button>
