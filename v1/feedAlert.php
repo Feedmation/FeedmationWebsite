b30b47bae510f9bac6cc72d02dee0416aec60614
Feedmation <jackson.wright69@gmail.com>
2014-10-12 20:58:11 -0500

    new code

--- v1/feedAlert.php
+++ v1/feedAlert.php
@@ -40,32 +40,7 @@ include_once 'loginFunctions.php';
 						{
 						
 						echo "tag result is true.";
-							/* 
-							
-								$tag_id = $row['tag_id'];
-								$petName = $row['pet_name'];
-								
-								$eventQ = "SELECT tag_id FROM $GLOBALS[schema].stats WHERE tag_id = $1";
-								$eventPrep = pg_prepare($dbconn,"event", $eventQ);
-								$eventResult = pg_execute($dbconn,"event",array($tag_id));
-								
-									//Have not eaten b/w two time slots
-									if(pg_num_rows($eventResult) ==0)
-									{
-								
-										//free result in case we want to use it again
-										//pg_free_result($prepResult);	
-										
-										$subject = "Feedmation - Feeding Reminder";
-										$message = "Hey your pet, $petName did not eat from your feeder $feederName . \n\n Please be sure to 
-													check on and make sure your pet receives a meal for today. 
-														.\n\n\n\n
-										- Feedmation";
-										$header = "From: info@feedmation.com \r\n";
-										$retval = mail($user, $subject, $message,$header);
-										echo "Email sent!!!! ".$tag_id;
-									} */
-									//pg_free_result($eventResult);
+						
 						}
 						pg_free_result($tagResult);
 					}
