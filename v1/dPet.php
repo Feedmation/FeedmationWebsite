807211bfcc5421e68eb3dce7d5495989321c0c55
Feedmation <jackson.wright69@gmail.com>
2014-10-13 15:06:31 -0500

    Ajax

--- v1/dPet.php
+++ v1/dPet.php
@@ -40,7 +40,7 @@
 		
 		alert("test ");
 		$.ajax({
-			url: 'dPet.php',
+			url: 'dPetHandler.php',
 			type: "GET",
 			data: {	populatePetsSelect: 'true',
 					feederId : feederId},
@@ -114,32 +114,13 @@
 	<select class="form-control" id='petSelect' name='pet'>
 		<?php /*populatePetsSelectBox();
 
-		$selectPets = "SELECT * FROM $GLOBALS[schema].rfid WHERE user_email = $1 and feeder_id = $2";
-		
-		$selectPetsPrep = pg_prepare($dbconn, "pets", $selectPets);
-		
-		if($selectPetsPrep) {
-			$petsResult = pg_execute($dbconn, "pets", array($_SESSION['user'], $feederID));
-		} else {
-			echo "Could not sanitize user name. Try again later.";
-		}
-		
-		if($petsResult)
-		{
-			$pets = '';
-			$i = 0;
-			while($row = pg_fetch_assoc($petsResult)) {
-				$pets.= "
-							<option value='$row[tag_id]'>$row[pet_name]</option>
-						   ";			
-				$i++;
-			}
-			pg_free_result($petsResult);*/
+		*/
 			?>
 			
 			<label for='pet'>Select a Pet:</label>
 	<select class="form-control" id='petSelect' name='pet'>
 		<?php 
+			/**/	
 		?>
 	</select>
 	<?
---
+++ v1/dPetHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+$feederID = $_REQUEST['feederID'];
+$selectPets = "SELECT * FROM $GLOBALS[schema].rfid WHERE user_email = $1 and feeder_id = $2";
+		
+$selectPetsPrep = pg_prepare($dbconn, "pets", $selectPets);
+		
+if($selectPetsPrep) {
+	$petsResult = pg_execute($dbconn, "pets", array($_SESSION['user'], $feederID));
+} else {
+		echo "Could not sanitize user name. Try again later.";
+}
+		
+if($petsResult)
+	{
+		$pets = '';
+		$i = 0;
+		while($row = pg_fetch_assoc($petsResult)) {
+			$pets.= "
+					<option value='$row[tag_id]'>$row[pet_name]</option>
+			 ";			
+			$i++;
+		}
+		pg_free_result($petsResult);
+		echo $pets;	
+	} else {
+		echo "Could not query for Pets. Try refreshing the page";
+	}
+?>
