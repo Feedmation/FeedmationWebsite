5b30add0160f746576a12d0d5b1a14437e07082c
Liuyuan Tan <tanliuyuan@aliyun.com>
2014-10-22 16:33:32 -0500

    testing chart.js

--- v1/pet_chart.php
+++ v1/pet_chart.php
@@ -31,14 +31,8 @@ include_once 'loginFunctions.php';
 </head>
 
 <body>
-	<!-- Canvas on which the pet chart will be drawn -->
-	<canvas id="petChart"></canvas>
-	
-	<!-- Draw chart -->
+	<!-- Chart.js global settings -->
 	<script>
-		var ctx = document.getElementById("petChart").getContext("2d");
-		var newChart = new Chart(ctx).PolarArea(data);
-		
 		Chart.defaults.global = {
 			// Boolean - Whether to animate the chart
 			animation: true,
@@ -159,6 +153,43 @@ include_once 'loginFunctions.php';
 			// Function - Will fire on animation completion.
 			onAnimationComplete: function(){}
 		}
+	</script>
+	
+	<!-- Canvas on which the pet chart will be drawn -->
+	<canvas id="petChart"></canvas>
+	
+	<!-- Draw chart -->
+	<script>
+	var data = {
+			labels: ["January", "February", "March", "April", "May", "June", "July"],
+			datasets: [
+				{
+					label: "My First dataset",
+					fillColor: "rgba(220,220,220,0.2)",
+					strokeColor: "rgba(220,220,220,1)",
+					pointColor: "rgba(220,220,220,1)",
+					pointStrokeColor: "#fff",
+					pointHighlightFill: "#fff",
+					pointHighlightStroke: "rgba(220,220,220,1)",
+					data: [65, 59, 80, 81, 56, 55, 40]
+				},
+				{
+					label: "My Second dataset",
+					fillColor: "rgba(151,187,205,0.2)",
+					strokeColor: "rgba(151,187,205,1)",
+					pointColor: "rgba(151,187,205,1)",
+					pointStrokeColor: "#fff",
+					pointHighlightFill: "#fff",
+					pointHighlightStroke: "rgba(151,187,205,1)",
+					data: [28, 48, 40, 19, 86, 27, 90]
+				}
+			]
+		};
+		
+		var ctx = document.getElementById("petChart").getContext("2d");
+		var newChart = new Chart(ctx).PolarArea(data);
+		
+		
 	</script>
 	
 </body>
