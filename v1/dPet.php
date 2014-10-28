14395d08bcda6f3ac123564dc7635f7d203afd79
Feedmation <jackson.wright69@gmail.com>
2014-10-28 15:49:50 -0500

    header out

--- v1/dPet.php
+++ v1/dPet.php
@@ -7,21 +7,7 @@
 	}
 ?>
 
-<!-- navbar -->
-	<nav class="navbar-default navbar-fixed-top">
-		<div class='container'>
-			<div class="navbar-header pull-left">
-				<p class="navbarText brand navbar-text"><?php echo "$_SESSION[fname]'s Feedmation Home"; ?></p>
-			</div>
-			<div class="navbar-header pull-right">
-				<p class="navbar-text">
-					<a href="changeP.php" class='btn btn-default pull-right'>Change Password</a>  
-					<a href="logout.php" class="btn btn-default pull-right">Logout</a>
-				</p>	
-			</div>
-		</div>
-	</nav>
-	<br><br><br>
+<br><br>
 
 <!DOCTYPE html>
 <html>
