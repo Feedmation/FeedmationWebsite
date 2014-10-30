6e927888822321336c518486a0188c5c19209dad
Feedmation <jackson.wright69@gmail.com>
2014-10-30 18:29:24 -0500

    changeP

--- v1/changeP.php
+++ v1/changeP.php
@@ -6,24 +6,33 @@ $loggedIn = empty($_SESSION['user']) ? false : $_SESSION['user'];
 <!DOCTYPE html>
 
 	<!-- navbar -->
-	<nav class="navbar-default navbar-fixed-top">
+	<nav id='navbar' role="navigation" class="navbar navbar-default navbar-fixed-top">
 		<div class='container'>
-			<div class="navbar-header pull-left">
-				<p class="navbarText brand navbar-text"><?php echo "$_SESSION[fname]'s Feedmation Home"; ?></p>
+			<div class="navbar-header">
+				<p class="navbarText pull-left navbar-text"><?php echo "$_SESSION[fname]'s Feedmation Home"; ?></p>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 			</div>
-			<ul class='nav navbar-nav pull-right'>	
-				<li class='dropdown'>
-				<a href="#" class="dropdown-toggle glyphicon glyphicon-cog" data-toggle="dropdown"></a>
-				<ul class="dropdown-menu" role="menu">
-					<li><a href="changeP.php">Change Password</a></li>
-					<li><a href='feedNow.php' id='feedNow' name='feedNow' onclick='feedNow(); return false;'>Feed Now!</a></li>
+			<div id="navbarCollapse" class="collapse navbar-collapse">
+				<ul id='navList' class='nav navbar-nav pull-right'>					
+					<li><a href='#' id='feedNow' name='feedNow' onclick='feedNow(); return false;'><span class='glyphicon glyphicon-time'></span> Feed Now!</a></li>
+					<li><a href='#' name='addFeeder' onclick="addFeeder(); return false;"><span class='glyphicon glyphicon-plus'></span> Add Feeder</a></li>
+					<li><a href='#' name='deleteFeeder' onclick="deleteFeeder(); return false;" ><span class='glyphicon glyphicon-trash'></span> Delete Feeder</a></li>
+					<li><a href='#' name='addPet' onclick="addPet(); return false;"><span class='glyphicon glyphicon-plus'></span> Add Pet</a></li>
+					<li><a href='#' name='addPet' onclick="editPet(); return false;"><span class='glyphicon glyphicon-cog'></span> Edit Pet</a></li>
+					<li><a href='dPet.php' id="deletePetBtn" name='deletePet'><span class='glyphicon glyphicon-trash'></span> Delete Pet</a></li>
 					<li class="divider"></li>
-					<li><a href="logout.php">Logout</a></li>				
-				</ul>				
-			</ul>
+					<li><a href="changeP.php"><span class='glyphicon glyphicon-user'></span> Change Password</a></li>
+					<li><a href="logout.php"><span class='glyphicon glyphicon-off'></span> Logout</a></li>				
+				</ul>
+			</div>
 		</div>	
 	</nav>
 	<!-- end navbar -->
+
 	<br><br><br>
 	
 	<html>
