05d68cdfcce67afd6724c8d4782c93d3f6bb4205
Feedmation <jackson.wright69@gmail.com>
2014-10-30 18:29:06 -0500

    hr

--- v1/editFeeder.php
+++ v1/editFeeder.php
@@ -68,6 +68,35 @@ include_once 'loginFunctions.php';
 
 </body>
 </html>
+	<!-- navbar -->
+	<nav id='navbar' role="navigation" class="navbar navbar-default navbar-fixed-top">
+		<div class='container'>
+			<div class="navbar-header">
+				<p class="navbarText pull-left navbar-text"><?php echo "$_SESSION[fname]'s Feedmation Home"; ?></p>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div id="navbarCollapse" class="collapse navbar-collapse">
+				<ul id='navList' class='nav navbar-nav pull-right'>					
+					<li><a href='#' id='feedNow' name='feedNow' onclick='feedNow(); return false;'><span class='glyphicon glyphicon-time'></span> Feed Now!</a></li>
+					<li><a href='#' name='addFeeder' onclick="addFeeder(); return false;"><span class='glyphicon glyphicon-plus'></span> Add Feeder</a></li>
+					<li><a href='#' name='deleteFeeder' onclick="deleteFeeder(); return false;" ><span class='glyphicon glyphicon-trash'></span> Delete Feeder</a></li>
+					<li><a href='#' name='addPet' onclick="addPet(); return false;"><span class='glyphicon glyphicon-plus'></span> Add Pet</a></li>
+					<li><a href='#' name='addPet' onclick="editPet(); return false;"><span class='glyphicon glyphicon-cog'></span> Edit Pet</a></li>
+					<li><a href='dPet.php' id="deletePetBtn" name='deletePet'><span class='glyphicon glyphicon-trash'></span> Delete Pet</a></li>
+					<li class="divider"></li>
+					<li><a href="changeP.php"><span class='glyphicon glyphicon-user'></span> Change Password</a></li>
+					<li><a href="logout.php"><span class='glyphicon glyphicon-off'></span> Logout</a></li>				
+				</ul>
+			</div>
+		</div>	
+	</nav>
+	<!-- end navbar -->
+
+
 
 	<?
 	$dbConn = dbconnect();
